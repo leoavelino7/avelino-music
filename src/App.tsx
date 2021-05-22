6c68a565bb9080216ca5430f98eb5f184fc26d48
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-05-22 14:36:09 -0300

    feat: adicionando e configurando Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Hi</h1>
+  <div className="bg-gray-300">
+    <h1 className="text-red-300">Hi</h1>
   </div>
 )
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/**.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#181818'
+      }
+    }
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+      opacity: ['disabled']
+    }
+  },
+  plugins: []
+}
