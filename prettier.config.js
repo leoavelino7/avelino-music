8c2b353573af502a652ca5f837e7eece29833d89
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-05-22 14:19:00 -0300

    style: configurando eslint, prettier e limpando codigos do projeto

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  printWidth: 140,
+  quoteProps: 'as-needed'
+}
