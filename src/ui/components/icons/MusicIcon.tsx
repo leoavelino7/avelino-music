6ec2aff7a779d9c8d453662dcf481dcca9943243
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-05-22 14:51:12 -0300

    feat: adicionando componentes de icones e arquivos publicos (audios e images)

---
+++ src/ui/components/icons/MusicIcon.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+const MusicIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
+    />
+  </svg>
+)
+
+export default MusicIcon
