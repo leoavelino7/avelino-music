11fa7131ab8cbcaec66c9dd0351020494480d0ce
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-05-22 16:21:50 -0300

    WIP: adicionando providers nas rotas Home e Playlist

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,29 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+
+// Providers
+import { PlaylistProvider } from './data/contexts/PlaylistContext'
+import { SpotifySearchResponseProvider } from './data/contexts/SpotifySearchResponseContext'
+import { TopTracksProvider } from './data/contexts/TopTracksContext'
+
+// Components
 import Loading from './ui/components/Loading'
 
+// Pages
 const Home = React.lazy(() => import('./ui/pages/Home'))
 const Playlist = React.lazy(() => import('./ui/pages/Playlist'))
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Suspense fallback={<Loading />}>
-      <Route path="/" exact component={Home} />
-      <Route path="/playlist" exact component={Playlist} />
+      <PlaylistProvider>
+        <SpotifySearchResponseProvider>
+          <TopTracksProvider>
+            <Route path="/" exact component={Home} />
+          </TopTracksProvider>
+        </SpotifySearchResponseProvider>
+        <Route path="/playlist" exact component={Playlist} />
+      </PlaylistProvider>
     </Suspense>
   </BrowserRouter>
 )
