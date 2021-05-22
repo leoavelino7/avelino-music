7c7925c56af113b45ab8713a74247d1ae30d94ca
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-05-22 16:38:33 -0300

    WIP: exibindo resultados da busca e checando se a musica se encontra ou nao na playlist

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 
 // Context
 import { SpotifySearchResponseContext } from '../../data/contexts/SpotifySearchResponseContext'
@@ -24,13 +24,18 @@ import SignInForm from '../components/SignInForm'
 import SearchForm from '../components/SearchForm'
 import ToogleTheme from '../components/ToggleTheme'
 import TitleHeader from '../components/TitleHeader'
+import { Item } from '../components/CardItem'
+import ListCards from '../components/ListCards'
 
 // Icons
 import SearchIcon from '../components/icons/SearchIcon'
 import MusicIcon from '../components/icons/MusicIcon'
+import PlayIcon from '../components/icons/PlayIcon'
+import StartIcon from '../components/icons/StartIcon'
 
 // Enum
 enum STATUS_TITLE {
+  SEARCH_FOR = 'Busque por seus principais artistas e músicas',
   WELCOME = 'Seja bem vindo ao Avelino Music. Entre com seu Spotify para poder montar a sua playlist favorita',
   SEARCHING_FOR = 'Buscando resultados para:',
   RESULT_FOR = 'Resultados para:',
@@ -54,6 +59,24 @@ const getTreatedTracks = (tracks: TrackObject[], playlistTracks: Track[]): Track
   })
 
   return treatedTracks
+}
+
+const convertTrackToCardType = (tracks: Track[], playlistTracks: Track[]): Item[] => {
+  const convertedTracks = tracks.map(track => ({
+    id: track.id,
+    image: {
+      url: track.image,
+      title: `${track.artistName} - ${track.musicName}`
+    },
+    mainText: track.musicName,
+    secondaryText: track.artistName,
+    active: playlistTracks.some(trackItem => trackItem.id === track.id),
+    LeftIcon: PlayIcon,
+    RightIcon: StartIcon,
+    rightIconColorWhenActive: 'text-green-500'
+  }))
+
+  return convertedTracks
 }
 
 // Others
@@ -87,10 +110,10 @@ const Home: React.FC = () => {
   const [selectedAudio] = useState<AudioPlayerProps>()
   const [isOpenTopTracks] = useState(false)
   const [queryApproved, setQueryApproved] = useState(true)
-  const [titleSearch] = useState('Busque por seus principais artistas e músicas')
+  const [titleSearch, setTitleSearch] = useState(STATUS_TITLE.SEARCH_FOR)
+  const [resultList, setResultList] = useState<Item[]>([])
 
   // Functions
-
   const handleSearch = (query = '', offset: number = spotifySearchState.offset, limit: number = spotifySearchState.limit): void => {
     const isApproved = query.trim().length > minCharsQuery
 
@@ -126,6 +149,35 @@ const Home: React.FC = () => {
         })
     }
   }
+
+  const handleSelect = (): void => {
+    console.log('handleSelect')
+  }
+
+  const handlePlayPreview = (): void => {
+    console.log('handlePlayPreview')
+  }
+
+  const toggleFavorite = (): void => {
+    console.log('toggleFavorite')
+  }
+
+  const onDragEnd = (): void => {
+    console.log('onDragEnd')
+  }
+
+  // Effects
+  useEffect(() => {
+    if (spotifySearchState.loading) {
+      setTitleSearch(STATUS_TITLE.SEARCHING_FOR)
+    } else if (spotifySearchState.success) {
+      setTitleSearch(STATUS_TITLE.RESULT_FOR)
+
+      setResultList(convertTrackToCardType(spotifySearchState.tracks, playlistState.tracks))
+    } else if (spotifySearchState.error) {
+      setTitleSearch(STATUS_TITLE.NOT_FOUND_RESULT_FOR)
+    }
+  }, [spotifySearchState.loading, spotifySearchState.success, spotifySearchState.error, spotifySearchState.tracks, playlistState.tracks])
 
   return (
     <div className="flex flex-col h-screen justify-between">
@@ -164,6 +216,17 @@ const Home: React.FC = () => {
                 <div className="flex flex-row justify-between items-center">
                   <TitleHeader title={titleSearch} description="" />
                 </div>
+                <ListCards
+                  listId="result-list"
+                  cards={resultList}
+                  actions={{
+                    onHandleClick: handleSelect,
+                    onHandleClickLeftIcon: handlePlayPreview,
+                    onHandleClickRightIcon: toggleFavorite
+                  }}
+                  onDragEnd={onDragEnd}
+                  isDragDisabled={false}
+                />
               </section>
             </>
           )}
